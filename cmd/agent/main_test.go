09d50ca900298d08d03fee553ea479a47e6917f0
ulia.makoveckaya <makowetskaya2011@yandex.ru>
2023-10-05 21:07:07 +0300

    Добавлен 3 инкремент

--- cmd/agent/main_test.go
+++ cmd/agent/main_test.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"errors"
+	"github.com/JuliyaMS/service-metrics-alerting/internal/headers"
 	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"regexp"
 	"runtime"
+	"strconv"
 	"testing"
 )
 
 func TestSendRequest(t *testing.T) {
+	srv := httptest.NewServer(headers.Router())
+	defer srv.Close()
+
 	var rtm runtime.MemStats
 	changeMetrics(rtm)
+
+	r, _ := regexp.Compile(`:\d+`)
+	port, _ := strconv.Atoi(r.FindAllString(srv.URL, -1)[0][1:])
 
 	tests := []struct {
 		nameTest string
@@ -18,8 +28,8 @@ func TestSendRequest(t *testing.T) {
 	}{
 		{
 			nameTest: "Test 1",
-			port:     8080,
-			want:     errors.New("request failed"),
+			port:     port,
+			want:     nil,
 		},
 		{
 			nameTest: "Test 2",
