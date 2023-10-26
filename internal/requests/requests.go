63402e60097eef7e7aa853d43551e81856a7a465
ulia.makoveckaya <makowetskaya2011@yandex.ru>
2023-10-26 16:20:18 +0300

    iter7: Исправлены функции для повторных запросов к серверу

--- internal/requests/requests.go
+++ internal/requests/requests.go
@@ -44,7 +44,6 @@ func sendRequestGaugeJSON(requestURL string) error {
 
 	for k, v := range metrics.GaugeAgent.ReturnValues() {
 		logger.Agent.Infow("Encode gauge metric", "addr", config.FlagRunAgAddr, "name", k, "value", v)
-		var res *http.Response
 		req.ID = k
 		req.Value = &v
 		reqByte, err := json.Marshal(req)
@@ -57,20 +56,26 @@ func sendRequestGaugeJSON(requestURL string) error {
 		logger.Agent.Infow("Send gauge metric", "addr", config.FlagRunAgAddr, "data", string(reqByte))
 		err = retry.Do(func() error {
 			var er error
-			res, er = client.Post(requestURL, "Content-Type: application/json", bytes.NewBuffer(reqByte))
+			res, er := client.Post(requestURL, "Content-Type: application/json", bytes.NewBuffer(reqByte))
+
+			if res != nil {
+				if erClose := res.Body.Close(); erClose != nil {
+					logger.Agent.Error(erClose.Error(), "event", "close response")
+					return erClose
+				}
+			}
 			return er
 		},
 			retry.Attempts(10),
-			retry.OnRetry(func(n uint, err error) { logger.Agent.Error("Retrying request after error: %v", err) }))
+			retry.OnRetry(func(n uint, err error) {
+				logger.Agent.Error("Retrying request after error: %v", err)
+			}))
 
 		if err != nil {
 			logger.Agent.Error(err.Error(), "event", "send request")
 			return errors.New("request failed")
 		}
-		if er := res.Body.Close(); er != nil {
-			logger.Agent.Error(er.Error(), "event", "close response")
-			return er
-		}
+
 	}
 	return nil
 }
@@ -79,7 +84,6 @@ func sendRequestCounterJSON(requestURL string) error {
 
 	req := metrics.Metrics{MType: "counter", ID: "PollCount", Delta: &metrics.PollCount}
 	client := http.Client{Timeout: time.Duration(60) * time.Second}
-	var res *http.Response
 
 	logger.Agent.Infow("Encode counter metric", "addr", config.FlagRunAgAddr, "name", "PollCount", "value", metrics.PollCount)
 
@@ -92,8 +96,14 @@ func sendRequestCounterJSON(requestURL string) error {
 
 	logger.Agent.Infow("Send counter metric", "addr", config.FlagRunAgAddr, "data", string(reqByte))
 	err = retry.Do(func() error {
-		var er error
-		res, er = client.Post(requestURL, "Content-Type: application/json", bytes.NewBuffer(reqByte))
+		res, er := client.Post(requestURL, "Content-Type: application/json", bytes.NewBuffer(reqByte))
+
+		if res != nil {
+			if erClose := res.Body.Close(); erClose != nil {
+				logger.Agent.Error(erClose.Error(), "event", "close response")
+				return erClose
+			}
+		}
 		return er
 	},
 		retry.Attempts(10),
@@ -103,10 +113,6 @@ func sendRequestCounterJSON(requestURL string) error {
 		logger.Agent.Error(err.Error(), "event", "send request")
 		return errors.New("request failed")
 	}
-	if er := res.Body.Close(); er != nil {
-		logger.Agent.Error(er.Error(), "event", "close response")
-		return er
-	}
 	return nil
 }
 
@@ -115,8 +121,14 @@ func SendRequestJSON() error {
 	requestURL := fmt.Sprintf("http://%s/update/", config.FlagRunAgAddr)
 
 	logger.Agent.Infow("Start send metrics")
-	err := sendRequestGaugeJSON(requestURL)
-	err = sendRequestCounterJSON(requestURL)
+	if err := sendRequestGaugeJSON(requestURL); err != nil {
+		logger.Agent.Error("Error in function sendRequestGaugeJSON")
+		return err
+	}
+	if err := sendRequestCounterJSON(requestURL); err != nil {
+		logger.Agent.Error("Error in function sendRequestCounterJSON")
+		return err
+	}
 
-	return err
+	return nil
 }
